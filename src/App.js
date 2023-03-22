2b7b3d5d7ce81a4a68e4de30c8727f686e870598
Sean Kempt <kempt101@gmail.com>
2023-03-22 18:10:31 -0500

    feat: add initial component structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './sass/reset.scss';
 import './sass/styles.scss';
+import PostCard from './components/PostCard';
 
 const App = () => (
   <div className="App">
     <header className="header">
-      <h1>Reddit Clone</h1>
+      <h1 className="header__title">Seddit</h1>
+      <input
+        type="text"
+        name="search"
+        value="Search Seddit"
+        className="header__search"
+      />
     </header>
     <main className="main">
       <p>this is the main section</p>
+      <PostCard />
     </main>
   </div>
 );
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,16 @@
+const PostCard = () => (
+  <div className="postcard__container">
+    <div className="post__upvotes">100</div>
+    <div className="post">
+      <p className="post__title">post title goes here</p>
+      <p className="post__body">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
+        consectetur quis. Voluptatibus veniam odio omnis labore, numquam dolorum
+        temporibus ea et reiciendis, ad ab repellat mollitia placeat. Harum, sed
+        odio?
+      </p>
+    </div>
+  </div>
+);
+
+export default PostCard;
