7c8837960a3fb4dbec042b2bee4b71ae77d65217
SeanKempt <kempt101@gmail.com>
2023-04-10 13:51:54 -0400

    feat: add title input for creating posts

--- src/components/CreatePostCard.jsx
+++ src/components/CreatePostCard.jsx
@@ -3,16 +3,27 @@ import PropTypes from 'prop-types';
 const CreatePostCard = ({ postInput, handlePostInputChange }) => (
   <div className="createpost">
     <i className="bi bi-person-circle" />
-    <input
-      type="text"
-      name="createpost"
-      id="createpost-text"
-      className="createpost__textinput"
-      value={postInput}
-      placeholder="What do you want to say?"
-      onChange={handlePostInputChange}
-    />
-    <button type="button">Post</button>
+    <form className="createpost_form">
+      <input
+        type="text"
+        name="createpost__title"
+        className="createpost__title"
+        placeholder="Whats the title?"
+      />
+      <textarea
+        name="createpost__body"
+        id="createpostbody"
+        cols="73"
+        rows="1"
+        value={postInput}
+        onChange={handlePostInputChange}
+        placeholder="What do you want to say?"
+        className="createpost__body"
+      />
+    </form>
+    <button type="button" className="createpost__button">
+      Post
+    </button>
   </div>
 );
 
